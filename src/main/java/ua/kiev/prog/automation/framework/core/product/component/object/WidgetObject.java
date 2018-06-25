89e92db3a2a774b650b8d022759a6e07abad71b5
differ9nt <differ9nt.ey9s@gmail.com>
2018-06-25 20:31:52 +0300

    Changes files

--- src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/WidgetObject.java
+++ src/main/java/ua/kiev/prog/automation/framework/core/product/component/object/WidgetObject.java
@@ -1,23 +1,27 @@
 package ua.kiev.prog.automation.framework.core.product.component.object;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.function.Function;
 
 /**
- * ////////////////////////////////////////////////////////// *
+ * //////////////////////////////////////////////////// *
  * Automation Framework                                       *
  * Automation Course for https://prog.kiev.ua/                *
  * ---------------------------------------------------------- *
  * Created by Yurii Voronenko                                 *
  * Email: yurii.voronenko@gmail.com                           *
- * ////////////////////////////////////////////////////////// *
+ * ////////////////////////////////////////////////////*
  */
 public class WidgetObject {
 
     private RemoteWebDriver _driver;
     private By              _locator;
-    private WebElement      _element;
+    //private WebElement      _element;
 
     public WidgetObject (RemoteWebDriver driver, By locator)
     {
@@ -25,23 +29,33 @@ public class WidgetObject {
         _locator    = locator;
     }
 
-    protected WebElement element()
+    protected WebElement element ()
     {
-        if (_element == null)
-            _element = _driver.findElement(_locator);
-        return _element;
+        try {
+            WebDriverWait wait = new WebDriverWait(this._driver, 30);
+            wait.until(new Function<WebDriver, Object>() {
+                @Override
+                public Object apply(WebDriver webDriver) {
+                    return webDriver.findElements(_locator).size() > 0;
+                }
+            });
+        } catch (Throwable e) {
+            throw new RuntimeException("Element not found");
+        }
+        return this._driver.findElement(_locator);
     }
-
-
 
     public void click ()
     {
         this.element().click();
     }
 
-    public String getText() {
+    public String getText ()
+    {
         return this.element().getText();
     }
+
+
 
 }
 
